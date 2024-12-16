107ba99f5e11f67e125795a74cfb43827f99c6cf
imadwaseem11 <sfixnow@gmail.com>
2024-12-16 15:21:12 +0530

    first commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://tms-frontend-nl8w.onrender.com"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
